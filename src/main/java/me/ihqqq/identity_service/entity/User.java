02d6ba7d49aa48a414f20526b882e81d844908fa
ihqqq <trnphi127@gmail.com>
2026-02-26 15:48:34 +0700

    unique-field

--- src/main/java/me/ihqqq/identity_service/entity/User.java
+++ src/main/java/me/ihqqq/identity_service/entity/User.java
@@ -18,6 +18,8 @@ public class User {
     @Id
     @GeneratedValue(strategy = GenerationType.UUID)
     String id;
+
+    @Column(name = "`username", unique = true, columnDefinition = "VARCHAR(255) COLLATE utf8mb4_unicode_ci")
     String username;
     String password;
     String firstName;
--- src/main/java/me/ihqqq/identity_service/service/UserService.java
+++ src/main/java/me/ihqqq/identity_service/service/UserService.java
@@ -15,6 +15,7 @@ import me.ihqqq.identity_service.exception.ErrorCode;
 import me.ihqqq.identity_service.mapper.UserMapper;
 import me.ihqqq.identity_service.repository.RoleRepository;
 import me.ihqqq.identity_service.repository.UserRepository;
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.security.access.prepost.PostAuthorize;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.security.core.context.SecurityContextHolder;
@@ -37,20 +38,20 @@ public class UserService {
     RoleRepository roleRepository;
 
     public UserResponse createUser(UserCreationRequest request) {
-        log.info("Service: Create User");
-
-        if(userRepository.existsByUsername(request.getUsername()))
-            throw new AppException(ErrorCode.USER_EXISTED);
-
         User user = userMapper.toUser(request);
         user.setPassword(passwordEncoder.encode(request.getPassword()));
-
         HashSet<Role> roles = new HashSet<>();
         roleRepository.findById(PredefinedRole.USER_ROLE).ifPresent(roles::add);
-
         user.setRoles(roles);
 
-        return userMapper.toUserResponse(userRepository.save(user));
+        try {
+            user = userRepository.save(user);
+        } catch (DataIntegrityViolationException e) {
+            throw new AppException(ErrorCode.USER_EXISTED);
+        }
+
+
+        return userMapper.toUserResponse(user);
 
     }
 
